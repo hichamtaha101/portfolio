bf418d6ff21ffe9039eb948e7b681f1359406c6d
hichamtaha <hichamtaha101@gmail.com>
2022-02-11 22:05:23 -0800

    home id

--- components/Main.vue
+++ components/Main.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="body-slider profile-section">
+  <div class="body-slider profile-section" id="#home">
 	  <div class="wrapper">
 		<div
 			class="body-introduction sm:pt-20"
